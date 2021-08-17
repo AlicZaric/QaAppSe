4d72b89c98f21b9a733beb9d5e84756023ba4e97
Alis <alis.zaric@gmail.com>
2021-08-17 15:59:36 +0300

    New Homework

---
+++ constants/login_page.py
@@ -0,0 +1,24 @@
+class LoginPageConstants:
+    """Store constants related to Login Page"""
+    SIGN_UP_USERNAME_XPATH = ".//input[@id='username-register']"
+    SIGN_UP_EMAIL_XPATH = ".//input[@id='email-register']"
+    SIGN_UP_PASSWORD_XPATH = ".//input[@id='password-register']"
+    SIGN_UP_BUTTON_XPATH = ".//button[@type='submit']"
+
+    # Sign in
+    SIGN_IN_USERNAME_XPATH = ".//input[@placeholder='Username']"
+    SIGN_IN_PASSWORD_XPATH = ".//input[@placeholder='Password']"
+    SIGN_IN_BUTTON_XPATH = ".//button[contains(text(), 'Sign In')]"
+
+    # Messages
+    INVALID_LOGIN_MESSAGE_TEXT = 'Invalid username / password'
+    INVALID_LOGIN_MESSAGE_XPATH = f".//div[contains(text(), '{INVALID_LOGIN_MESSAGE_TEXT}')]"
+    INVALID_REG_MESSAGE_XPATH = ".//div[@class='alert alert-danger small liveValidateMessage liveValidateMessage--visible']"
+    ERROR_MESSAGE_MAIL_TEXT = 'You must provide a valid email address.'
+    ERROR_MESSAGE_MAIL_XPATH = ".//div[contains(text(), 'You must provide a valid email address')]"
+    ERROR_MESSAGE_PASSWORD_TEXT = 'Password must be at least 12 characters.'
+    ERROR_MESSAGE_PASSWORD_XPATH = ".//div[contains(text(), 'Password must be at least 12 characters')]"
+    ERROR_MESSAGE_USERNAME_TEXT = 'Username must be at least 3 characters.'
+    ERROR_MESSAGE_USERNAME_XPATH = ".//div[contains(text(), 'Username must be at least 3 characters.')]"
+    ERROR_MESSAGE_MAIL_USED_TEXT = 'That email is already being used.'
+    ERROR_MESSAGE_MAIL_USED_XPATH = ".//div[contains(text(), 'That email is already being used.')]"
---
+++ constants/profile_page.py
@@ -0,0 +1,3 @@
+class ProfilePage:
+    """Store constants related to Profile Page"""
+    HELLO_MESSAGE_TEXT = "Hello {lower_username}, your feed is empty."
